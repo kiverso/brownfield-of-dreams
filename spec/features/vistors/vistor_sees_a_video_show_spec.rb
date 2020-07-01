c2b1cb93798790f06a4dbb67bd23698f48fa4012
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-06-30 21:47:23 -0600

    change tutorial show to not allow visitor to view classroom tutorials

--- spec/features/vistors/vistor_sees_a_video_show_spec.rb
+++ spec/features/vistors/vistor_sees_a_video_show_spec.rb
@@ -13,4 +13,22 @@ describe 'visitor sees a video show' do
     expect(page).to have_content(video.title)
     expect(page).to have_content(tutorial.title)
   end
+
+  it 'visitor tries to view a video for classroom content' do
+    tutorial1 = create(:classroom_tutorial)
+    tutorial2 = create(:tutorial)
+
+    video1 = create(:video, tutorial_id: tutorial1.id)
+    video2 = create(:video, tutorial_id: tutorial2.id)
+
+    visit '/'
+
+    expect(page).to_not have_link(tutorial1.title)
+    visit tutorial_path(tutorial1)
+
+    expect(current_path).to eq(tutorial_path(tutorial1))
+    expect(page).to_not have_content(video1.title)
+    expect(page).to_not have_content(tutorial1.title)
+    expect(page).to have_content("The page you were looking for doesn't exist.")
+  end
 end
