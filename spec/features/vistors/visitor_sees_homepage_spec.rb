a4b05b314d85ef1d4628d5a2d4556b49952f7819
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-06-30 21:34:16 -0600

    Add test for logged in user to see both classroom and non classroom content

--- spec/features/vistors/visitor_sees_homepage_spec.rb
+++ spec/features/vistors/visitor_sees_homepage_spec.rb
@@ -23,7 +23,7 @@ describe 'Visitor' do
       end
     end
 
-    it 'will not show classroom material' do
+    it 'will not show classroom material to a visitor' do
       tutorial1 = create(:classroom_tutorial)
       tutorial2 = create(:tutorial)
 
@@ -37,6 +37,29 @@ describe 'Visitor' do
 
       expect(page).to_not have_content(tutorial1.title)
       expect(page).to_not have_content(tutorial1.description)
+      
+      expect(page).to have_content(tutorial2.title)
+      expect(page).to have_content(tutorial2.description)
+    end
+
+    it 'will show classroom material to a logged in user' do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      tutorial1 = create(:classroom_tutorial)
+      tutorial2 = create(:tutorial)
+
+      video1 = create(:video, tutorial_id: tutorial1.id)
+      video2 = create(:video, tutorial_id: tutorial1.id)
+      video3 = create(:video, tutorial_id: tutorial2.id)
+      video4 = create(:video, tutorial_id: tutorial2.id)
+      
+      visit root_path
+
+      expect(page).to have_css('.tutorial', count: 2)
+
+      expect(page).to have_content(tutorial1.title)
+      expect(page).to have_content(tutorial1.description)
       
       expect(page).to have_content(tutorial2.title)
       expect(page).to have_content(tutorial2.description)
