596c883994c3e7557e69cb285a1d833c2f804da5
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-06-30 21:23:30 -0600

    Add authorized for user method to tutorial model

--- spec/models/tutorial_spec.rb
+++ spec/models/tutorial_spec.rb
@@ -1,4 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Tutorial, type: :model do
+  describe 'class methods' do
+    it 'authorized_for_viewer' do
+      user1 = create(:user)
+      visitor = nil
+
+      tutorial1 = create(:classroom_tutorial)
+      tutorial2 = create(:tutorial)
+
+      video1 = create(:video, tutorial_id: tutorial1.id)
+      video2 = create(:video, tutorial_id: tutorial1.id)
+      video3 = create(:video, tutorial_id: tutorial2.id)
+      video4 = create(:video, tutorial_id: tutorial2.id)
+\
+      expect(Tutorial.authorized_for_viewer(user1)).to eq([tutorial1, tutorial2])
+      expect(Tutorial.authorized_for_viewer(visitor)).to eq([tutorial2])
+    end
+  end
 end
