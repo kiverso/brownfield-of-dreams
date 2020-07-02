9d0d2e8bdf0e22670ef22cadacd08cad0692ac45
Chase.Gaddis <cgaddis36@gmail.com>
2020-07-02 17:50:13 -0600

    linespace cleanup
    Co-Authored-By: Kyle Iverson <kiverso@users.noreply.github.com>

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,12 +1,12 @@
 class GithubService
 
   def find_data(user, data)
-    repos = get_json("user/#{data}", user)
+    data = get_json("user/#{data}", user)
   end
 
   private
 
-  def get_json(url, user)    
+  def get_json(url, user)
     response = conn.get(url, nil, {Authorization: "token #{user.token}"})
     JSON.parse(response.body, symbolize_names: true)
   end
