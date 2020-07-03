5aaf2fb63696a3c8262e11d0701df1f04bd1b1a4
Chase.Gaddis <cgaddis36@gmail.com>
2020-07-03 15:56:42 -0600

    create oauth spec
    Co-Authored-By: Kyle Iverson <kiverso@users.noreply.github.com>

---
+++ spec/features/user/user_github_oauth_login_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'as a logged in user' do
+  before(:each) do
+    @user = User.create!(email: 'user@example.com', first_name: 'user1', last_name: 'Jenkins', password: '12345')
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+  end
+  it 'I can connect with my github account' do
+
+    visit dashboard_path
+
+    expect(page).to have_link("Connect to Github")
+  end
+end
+
+# As a user
+# When I visit /dashboard
+# Then I should see a link that is styled like a button that says "Connect to Github"
+# And when I click on "Connect to Github"
+# Then I should go through the OAuth process
+# And I should be redirected to /dashboard
+# And I should see all of the content from the previous Github stories (repos, followers, and following)
