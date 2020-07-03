336454776b5ee4a0d3ac8989173560acb5038def
Chase.Gaddis <cgaddis36@gmail.com>
2020-07-03 13:04:49 -0600

    import playlist and playlist item methods
    Co-Authored-By: Kyle Iverson <kiverso@users.noreply.github.com>

--- app/services/youtube_service.rb
+++ app/services/youtube_service.rb
@@ -5,10 +5,23 @@ class YoutubeService
     get_json('youtube/v3/videos', params)
   end
 
+  def import_playlist_items(id)
+    params = { part: 'snippet', playlistId: id, maxResults: 50 }
+
+    get_json('youtube/v3/playlistItems', params)
+  end
+
+  def import_playlist(id)
+    params = { part: 'snippet', maxResults: 50, id: id }
+
+    parsed = get_json('youtube/v3/playlists', params)
+    parser = parsed[:items][0][:snippet]
+  end
   private
 
   def get_json(url, params)
     response = conn.get(url, params)
+
     JSON.parse(response.body, symbolize_names: true)
   end
 
