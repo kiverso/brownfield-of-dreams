eb5c9626b451f3c0cd0fecff8a543c22a1e9418a
Chase.Gaddis <cgaddis36@gmail.com>
2020-07-06 08:41:32 -0600

    remove description validation

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -1,5 +1,5 @@
 class Tutorial < ApplicationRecord
-  validates_presence_of :title, :description, :thumbnail
+  validates_presence_of :title, :thumbnail
   has_many :videos, -> { order(position: :ASC) }, inverse_of: :tutorial
   acts_as_taggable_on :tags, :tag_list
   accepts_nested_attributes_for :videos
--- spec/models/tutorial_spec.rb
+++ spec/models/tutorial_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Tutorial, type: :model do
   describe 'validations' do
     it {should validate_presence_of(:title)}
-    it {should validate_presence_of(:description)}
     it {should validate_presence_of(:thumbnail)}
   end
   describe 'class methods' do
