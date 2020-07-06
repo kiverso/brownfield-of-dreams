45a8ffe0703318fd0f2df5280d213bc4ab0bbf64
Chase.Gaddis <cgaddis36@gmail.com>
2020-07-06 14:43:38 -0600

    move service into helper method

--- app/controllers/admin/tutorials_controller.rb
+++ app/controllers/admin/tutorials_controller.rb
@@ -5,17 +5,7 @@ class Admin::TutorialsController < Admin::BaseController
 
   def create
     if id = params[:tutorial][:playlist_id]
-      service = YoutubeService.new
-      playlist_data = service.import_playlist(id)
-      @playlist_video_data = service.import_playlist_items(id, nil)
-
-      @tutorial = Tutorial.create!(title: playlist_data[:title], description: playlist_data[:description], thumbnail: playlist_data[:thumbnails][:default][:url], playlist_id: id)
-      @playlist_video_data[:items].each {|video| @tutorial.import_video(video)}
-
-      while @playlist_video_data[:nextPageToken]
-        @playlist_video_data = service.import_playlist_items(id, @playlist_video_data[:nextPageToken])
-        @playlist_video_data[:items].each {|video| @tutorial.import_video(video)}
-      end
+      youtube_service(id)
     else
       @tutorial = Tutorial.create(tutorial_params)
     end
@@ -49,6 +39,20 @@ class Admin::TutorialsController < Admin::BaseController
   end
 
   private
+
+  def youtube_service(id)
+    service = YoutubeService.new
+    playlist_data = service.import_playlist(id)
+    @playlist_video_data = service.import_playlist_items(id, nil)
+
+    @tutorial = Tutorial.create!(title: playlist_data[:title], description: playlist_data[:description], thumbnail: playlist_data[:thumbnails][:default][:url], playlist_id: id)
+    @playlist_video_data[:items].each {|video| @tutorial.import_video(video)}
+
+    while @playlist_video_data[:nextPageToken]
+      @playlist_video_data = service.import_playlist_items(id, @playlist_video_data[:nextPageToken])
+      @playlist_video_data[:items].each {|video| @tutorial.import_video(video)}
+    end
+  end
 
   def tutorial_params
     params.require(:tutorial).permit(:tag_list, :title, :description, :thumbnail)
