2f9c27bde75ece3c5fccec2b4ef455422f4dc66c
Chase.Gaddis <cgaddis36@gmail.com>
2020-07-06 18:08:24 -0600

    change link to button for deleting a tutorial

--- spec/features/admin/admin_can_delete_tutorial_spec.rb
+++ spec/features/admin/admin_can_delete_tutorial_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 feature "An admin can delete a tutorial" do
   scenario "and it should no longer exist" do
     admin = create(:admin)
-    create_list(:tutorial, 2)
+    tutorials = create_list(:tutorial, 2)
+    tutorial = tutorials[0]
+    video = Video.create!(title: 'New Video', description: 'This is the newest video on Youtube.', tutorial_id: tutorial.id)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
@@ -12,8 +14,10 @@ feature "An admin can delete a tutorial" do
     expect(page).to have_css('.admin-tutorial-card', count: 2)
 
     within(first('.admin-tutorial-card')) do
-      click_link 'Delete'
+      click_button 'Delete'
     end
+
+    expect(tutorial.videos).to be_empty
 
     expect(page).to have_css('.admin-tutorial-card', count: 1)
   end
