a392c7078dd21f0cc7ad65b35dd335254a2d341c
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-07-08 17:19:27 -0600

    Make get_json public

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -3,13 +3,12 @@ class GithubService
     get_json("user/#{data}", user)
   end
 
-  private
-
   def get_json(url, user)
     response = conn.get(url, nil, { Authorization: "token #{user.token}" })
     JSON.parse(response.body, symbolize_names: true)
   end
 
+  private
   def conn
     Faraday.new(url: 'https://api.github.com')
   end
