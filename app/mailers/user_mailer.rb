62aa0c790919de54052d5285cc477eceb66c9071
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-07-08 17:30:58 -0600

    Update mailer to use invite

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,9 +5,10 @@ class UserMailer < ApplicationMailer
 
 
 
-  def invite(invitee)
-    @invitee = invitee
+  
+  def invite(invite)
+    @invite = invite
 
-    mail(to: @invitee.email, subject:  'Invitation to join Turing Tutorials')
+    mail(to: @invite.recipient_email, subject:  'Invitation to join Turing Tutorials')
   end
 end
