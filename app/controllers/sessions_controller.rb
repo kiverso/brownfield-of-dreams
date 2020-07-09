3b05a8994cafe0025a738244c4e80833758d0aff
Chase.Gaddis <cgaddis36@gmail.com>
2020-07-08 19:35:54 -0600

    fix update_column for api call

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,8 +17,8 @@ class SessionsController < ApplicationController
   def update
     token = request.env['omniauth.auth'][:credentials][:token]
     url = request.env['omniauth.auth'][:extra][:raw_info][:html_url]
-    
-    current_user.update_attributes(token: token, url: url)
+
+    current_user.update_columns(:token => token, :url => url)
     current_user.save
     flash[:success] = 'Connected to Github!'
     redirect_to dashboard_path
