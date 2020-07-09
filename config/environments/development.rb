93e5c7c31fc79e25941448f5d0c29e4fbfce5a94
Chase.Gaddis <cgaddis36@gmail.com>
2020-07-08 18:22:31 -0600

    update to heroku.com

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
   authentication:       'plain',
   enable_starttls_auto: true  }
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { :host => 'heroku' }
+  config.action_mailer.default_url_options = { :host => 'heroku.com' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
