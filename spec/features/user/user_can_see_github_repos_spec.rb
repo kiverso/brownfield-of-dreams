37baf55064fe1d434ab612584d6db8452af70f46
Chase.Gaddis <cgaddis36@gmail.com>
2020-07-09 07:09:08 -0600

    instantiate webmock gem for testing efficiency

--- spec/features/user/user_can_see_github_repos_spec.rb
+++ spec/features/user/user_can_see_github_repos_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "As a logged in user, when i click on the github section" do
     @happy = User.create!(email: 'happymadison@example.com', first_name: 'Happy', last_name: 'Gilmore', password: '12345', token: ENV["github_api_token_c"])
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@happy)
   end
-  it 'can view 5 repos from their github account' do
+  it 'can view 5 repos from their github account', :vcr do
     visit '/dashboard'
 
     expect(page).to have_css('#repo', count: 5)
